51ee276d66c6c48d45e55bafa4392b9b7e031e75
Hasan <https://github.com/HasanRzayev/Hasan.git>
2022-03-14 09:58:17 +0300

    yeniiii

---
+++ GallaryWPF/ImagePage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GallaryWPF
+{
+    /// <summary>
+    /// Interaction logic for ImagePage.xaml
+    /// </summary>
+    
+    public partial class ImagePage : Page
+    {
+        private string source;
+
+        public string Adress
+        {
+            get { return source; }
+            set { source = value; OnPropertyChanged(); }
+        }
+
+      
+        public List<Image_gallery> images { get; set; }
+        public int index { get; set; }
+        public ImagePage()
+        {
+            InitializeComponent();
+
+
+        }
+        public ImagePage(List<Image_gallery> images,int index)
+        {
+            InitializeComponent();
+
+            //List<Image_gallery> images = new List<Image_gallery>();
+
+            Adress = images[index].source;
+            this.images = images;
+            this.index = index;  
+            DataContext = this;
+        }
+          public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void evvelki_Click(object sender, RoutedEventArgs e)
+        {
+            if (index - 1 < 0) index=images.Count-1;
+            else index -=1;
+            Adress = images[index].source;
+     
+        }
+    }
+}
--- GallaryWPF/MainWindow.xaml.cs
+++ GallaryWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,26 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
+public class Image_gallery
+{
+    public Image_gallery(BitmapImage image, string source)
+    {
+        this.image = image;
+        this.source = source;
+    }
+
+    public BitmapImage image { get; set; }
+
+    public  string source { get; set; }
+}
 namespace GallaryWPF
 {
     /// <summary>
@@ -21,24 +37,85 @@ namespace GallaryWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        List<Image_gallery> images = new List<Image_gallery>();
         public MainWindow()
         {
             InitializeComponent();
-            List<Image> images = new List<Image>();
-            galeryicindekiler.ItemsSource=images;
+         
+ 
             DataContext = this;
         }
-
+        public static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog op = new OpenFileDialog();
-            op.Title = "Select a picture";
-            op.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
-            if (op.ShowDialog() == true)
+
+            int count = 0;
+            string adress=null;
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
             {
-               
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result==System.Windows.Forms.DialogResult.OK) adress = dialog.SelectedPath;
+
             }
+            if (adress != null)
+            {
+                string[] files = Directory.GetFiles($@"{adress}");
+                foreach (var f in files)
+                {
+                    if (ImageExtensions.Contains(System.IO.Path.GetExtension(f).ToUpperInvariant()))
+                    {
+
+                    }
+                    else { count++; }
+                }
+
+                if (count == 0)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        BitmapImage myBitmapImage = new BitmapImage();
+
+                      
+                        myBitmapImage.BeginInit();
+                        myBitmapImage.UriSource = new Uri($@"{files[i]}");
+
+
+                        myBitmapImage.DecodePixelWidth = 200;
+                        myBitmapImage.EndInit();
+                        
+                        Image lazimli = new Image();
+                        Image_gallery sadesekil = new Image_gallery(myBitmapImage, files[i]);
+                        images.Add(sadesekil);
+
+
+                    }
+
+
+                    galeryicindekiler.ItemsSource = images;
+                }
+            }
+            
+        }
+
+        private void Listvievform_Click(object sender, RoutedEventArgs e)
+        {
+            Style style = this.FindResource("List") as Style;
+           
+            qallery.Style= style;
+        }
+
+        private void Image_Click(object sender, RoutedEventArgs e)
+        {
+         if(sender is Button btn)
+            {
+                qallery.Visibility = Visibility.Hidden;
+                ikinci.Width = 1000;
+                ikinci.Height = 800;
+                ImagePage imagepage=new ImagePage(images, images.IndexOf(images.FindAll(n => n.source == btn.Tag.ToString()).ToList()[0]));
+      
+                ikinci.Navigate(imagepage);
+            }
+
         }
     }
 }
